19aeb31222fb983c26a59336eb1f774aa0aff6a2
Muhammad Haikal Bintang <mhaikalbintang.work@gmail.com>
2025-01-17 23:37:22 +0700

    Update home.blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,58 +10,50 @@
 <body class="antialiased mx-auto bg-[#111827]">
   <div class="flex h-screen">
 
-    <aside class="w-1/4 h-screen relative overflow-y-auto pl-32 text-white bg-[#111523]">
+    <aside class="w-1/4 h-screen relative overflow-y-auto pl-28 text-white bg-[#111523]">
       <x-logo></x-logo>
-      <div class="pt-32 min-w-36">
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
-        <p>Lorem, ipsum.</p>
+      <div class="pt-[120px] max-w-56 leading-7 tracking-wide font-medium">
+        <h2 class="text-lg pt-2"><span class="text-red-500 text-2xl">#</span> Status</h2>
+        <div class="text-gray-100 pl-4">
+          <p><span class="text-red-500 text-xl">#</span> Baru</p>
+          <p><span class="text-red-500 text-xl">#</span> Bekas</p>
+        </div>
+        <h2 class="text-lg pt-2">Gender</h2>
+        <div class="text-gray-100 pl-4">
+          <p>Pria</p>
+          <p>Wanita</p>
+          <p>Unisex</p>
+        </div>
+        <h2 class="text-lg pt-2">Kategori</h2>
+        <div class="text-gray-100 pl-4">
+          <p>Tas</p>
+          <p>Baju</p>
+          <p>Celana</p>
+          <p>Rok</p>
+          <p>Sepatu</p>
+          <p>Topi</p>
+          <p>Kacamata</p>
+          <p>Handphone</p>
+          <p>Televisi</p>
+          <p>Monitor</p>
+          <p>Laptop</p>
+          <p>Keyboard</p>
+          <p>Mouse</p>
+          <p>Kursi</p>
+          <p>Sofa</p>
+          <p>Kasur</p>
+        </div>
+        <h2 class="text-lg pt-2">Kategori</h2>
+        <div class="text-gray-100 pl-4">
+          <p>Baru</p>
+          <p>Bekas</p>
+        </div>
+        <p class="pt-32 text-base text-gray-300"><span class="text-red-500 text-lg">WA</span><span class="text-purple-500 text-lg">:</span> +6287877901515</p>
+        <p class="pt-2 pb-8 text-base text-gray-300"><span class="text-red-500 text-lg">Lokasi</span><span class="text-purple-500 text-lg">:</span> Jl. KH Abdurrahman No. 15, RT 01/01, Pondok Jaya, Cipayung, Depok</p>
       </div>
     </aside>
   
-    <main class="w-3/4 h-full overflow-y-auto pl-8 pr-40">
+    <main class="w-3/4 h-full overflow-y-auto pl-8 pr-48">
       <x-navbar></x-navbar>
       <x-header><span class="text-3xl text-gray-100">Welcome to</span> <span class="font-bold">Toko Laravel</span>! <span class="text-2xl text-gray-100">Tempat jual beli barang bekas.</span></x-header>
 
